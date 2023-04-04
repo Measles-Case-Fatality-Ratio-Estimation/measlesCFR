f2422b3822999e73e04e0a3480130e74b518dad4
everdyke <everdyke@uw.edu>
2023-04-04 10:22:56 -0700

    Test onload

--- R/zzz.R
+++ R/zzz.R
@@ -2,14 +2,19 @@ mrbrt <- NULL
 
 .onLoad <- function(libname, pkgname) {
   ## Download pip packages for  dill and mrtool
-  reticulate::virtualenv_create("measles_conda")
-  reticulate::use_virtualenv("measles_conda", required = TRUE)
-  env <- reticulate::py_config()$python
-  reticulate::use_python(env, required = T)
-  Sys.setenv("RETICULATE_PYTHON" = env)
-  reticulate::py_config()
-  reticulate::py_install("measles_conda", packages = c("mrtool==0.1.0"), pip = TRUE)
-  reticulate::py_install("measles_conda", packages = c("dill==0.3.6"), pip = TRUE)
+  # reticulate::virtualenv_create("measles_conda")
+  # reticulate::use_virtualenv("measles_conda", required = TRUE)
+  # env <- reticulate::py_config()$python
+  # reticulate::use_python(env, required = T)
+  # Sys.setenv("RETICULATE_PYTHON" = env)
+  # reticulate::py_config()
+  # reticulate::py_install("measles_conda", packages = c("mrtool==0.1.0"), pip = TRUE)
+  # reticulate::py_install("measles_conda", packages = c("dill==0.3.6"), pip = TRUE)
+  # mrbrt <<- reticulate::import("mrtool", delay_load = TRUE)
+
+  reticulate::py_install(packages = c("mrtool==0.1.0"), pip = TRUE)
+  reticulate::py_install(packages = c("dill==0.3.6"), pip = TRUE)
+  # reticulate::use_virtualenv("r-reticulate")
   mrbrt <<- reticulate::import("mrtool", delay_load = TRUE)
 
   ## Load baseline and vaccination covariate CSVs (used in predictCFR.R)

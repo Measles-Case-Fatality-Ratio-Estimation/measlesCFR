5ecf3aef9f77fccf6067ec2e31ebde99604e945c
everdyke <everdyke@uw.edu>
2023-04-19 10:39:28 -0700

    Update displayCovariate

--- R/displayCovariates.R
+++ R/displayCovariates.R
@@ -27,33 +27,49 @@
 #' @export
 displayCovariates <- function(country, inputDF=NULL) {
   if (missing(inputDF)) {
-    ihme_loc_id = NULL
-    year_id = NULL
-    origin = NULL
-    value = NULL
-    value_origin = NULL
-    Z = NULL
-
     inputDF_original <- fread(measlesCFR::square_covariate_set)
 
     inputDF_original <- subset(inputDF_original, ihme_loc_id == country & year_id %in% c(1990:2019))
     inputDF_original$year <- inputDF_original$year_id
 
-    inputDF_original <- subset(inputDF_original, select=c('year', 'vitA_deficiency', 'maternal_education', 'u5mr', 'mcv1', 'prop_urban'))
-    inputDF_original$vitA_deficiency_origin <- 'Default'
-    inputDF_original$maternal_education_origin <- 'Default'
-    inputDF_original$u5mr_origin <- 'Default'
-    inputDF_original$mcv1_origin <- 'Default'
-    inputDF_original$prop_urban_origin <- 'Default'
+    incidence_df <- fread(measlesCFR::baseline_covariates)
+    incidence_df <- subset(incidence_df, country == country & year %in% c(1990:2019))
+    mean_incidence <- as.numeric(fread(measlesCFR::incidence_mean))
+    sd_incidence <- as.numeric(fread(measlesCFR::incidence_sd))
+    incidence_df$incidence <- exp((incidence_df$incidence_standardized * sd_incidence) + mean_incidence ) / (1 + exp((incidence_df$incidence_standardized * sd_incidence) + mean_incidence ))
 
-    fake_incidence <- seq(0.000001, 0.1, length=30)
+    incidence_df <- subset(incidence_df, select=c("country", "year", "incidence"))
+
+    inputDF_original <- merge(inputDF_original, incidence_df, by.x=c('ihme_loc_id', 'year_id'), by.y=c('country', 'year'))
 
 
-    inputDF_original$incidence <- fake_incidence
-    inputDF_original$incidence_origin <- 'User-specified'
-  }
-  else {
+  } else {
     inputDF_original <- inputDF
+
+    mean_mcv1 <- as.numeric(fread(measlesCFR::mcv_mean))
+    sd_mcv1 <- as.numeric(fread(measlesCFR::mcv_sd))
+    inputDF_original$mcv1 <- (inputDF_original$mcv1_standardized * sd_mcv1) + mean_mcv1
+
+    mean_maternal_education <- as.numeric(fread(measlesCFR::maternal_education_mean))
+    sd_maternal_education <- as.numeric(fread(measlesCFR::maternal_education_sd))
+    inputDF_original$maternal_education <- (inputDF_original$maternal_education_standardized * sd_maternal_education) + mean_maternal_education
+
+    mean_incidence <- as.numeric(fread(measlesCFR::incidence_mean))
+    sd_incidence <- as.numeric(fread(measlesCFR::incidence_sd))
+    inputDF_original$incidence <- exp((inputDF_original$incidence_standardized * sd_incidence) + mean_incidence ) / (1 + exp((inputDF_original$incidence_standardized * sd_incidence) + mean_incidence ))
+
+    mean_prop_urban <- as.numeric(fread(measlesCFR::prop_urban_mean))
+    sd_prop_urban <- as.numeric(fread(measlesCFR::prop_urban_sd))
+    inputDF_original$prop_urban <- (inputDF_original$prop_urban_standardized * sd_prop_urban) + mean_prop_urban
+
+    mean_u5mr <- as.numeric(fread(measlesCFR::u5mr_mean))
+    sd_u5mr <- as.numeric(fread(measlesCFR::u5mr_sd))
+    inputDF_original$u5mr <- (inputDF_original$u5mr_standardized * sd_u5mr) + mean_u5mr
+
+    mean_vitA <- as.numeric(fread(measlesCFR::vitA_mean))
+    sd_vitA <- as.numeric(fread(measlesCFR::vitA_sd))
+    inputDF_original$vitA_deficiency <- (inputDF_original$vitA_standardized * sd_vitA) + mean_vitA
+
   }
 
   # -----------------------------------------------------------------------------------------------
@@ -62,59 +78,41 @@ displayCovariates <- function(country, inputDF=NULL) {
 
   country <- country
 
-  d <- subset(inputDF_original, select=c('year', 'vitA_deficiency', 'maternal_education', 'u5mr', 'mcv1', 'prop_urban', 'incidence',
-                                         'vitA_deficiency_origin', 'maternal_education_origin', 'u5mr_origin', 'mcv1_origin', 'prop_urban_origin', 'incidence_origin'))
+  d <- subset(inputDF_original, select=c('year', 'vitA_deficiency', 'maternal_education', 'u5mr', 'mcv1', 'prop_urban', 'incidence'))
 
-  d <- melt(d, id = c('year','vitA_deficiency_origin', 'maternal_education_origin', 'u5mr_origin', 'mcv1_origin', 'prop_urban_origin', 'incidence_origin'))
-
-  d$value_origin <- ifelse(d$variable == "vitA_deficiency", d$vitA_deficiency_origin,
-                           ifelse(d$variable == "maternal_education", d$maternal_education_origin,
-                                  ifelse(d$variable == "mcv1", d$mcv1_origin,
-                                         ifelse(d$variable == "u5mr", d$u5mr_origin,
-                                                ifelse(d$variable == "prop_urban", d$prop_urban_origin,
-                                                       ifelse(d$variable == "incidence", d$incidence_origin,NA))))))
+  d <- melt(d, id = c('year'))
 
   ## set max limits for lines
   limits_df <- fread(measlesCFR::square_covariate_set)
 
-  limits_df <- subset(limits_df, ihme_loc_id == country & year_id %in% unique(inputDF_original$year))
+  limits_df <- subset(limits_df, ihme_loc_id == country & year_id %in% c(1990:2019))
   limits_df$year <- limits_df$year_id
 
-  limits_df <- subset(limits_df, select=c('year', 'vitA_deficiency', 'maternal_education', 'u5mr', 'mcv1', 'prop_urban'))
+  incidence_df <- fread(measlesCFR::baseline_covariates)
+  incidence_df <- subset(incidence_df, country == country & year %in% c(1990:2019))
+  mean_incidence <- as.numeric(fread(measlesCFR::incidence_mean))
+  sd_incidence <- as.numeric(fread(measlesCFR::incidence_sd))
+  incidence_df$incidence <- exp((incidence_df$incidence_standardized * sd_incidence) + mean_incidence ) / (1 + exp((incidence_df$incidence_standardized * sd_incidence) + mean_incidence ))
+
+  incidence_df <- subset(incidence_df, select=c("country", "year", "incidence"))
+
+  limits_df <- merge(limits_df, incidence_df, by.x=c('ihme_loc_id', 'year_id'), by.y=c('country', 'year'))
+
+  limits_df <- subset(limits_df, select=c('year', 'vitA_deficiency', 'maternal_education', 'u5mr', 'mcv1', 'prop_urban', 'incidence'))
 
   max_limits <- data.frame(variable = c("vitA_deficiency", "maternal_education", "mcv1", "u5mr", "prop_urban", "incidence"),
-                           Z = c(max(limits_df$vitA_deficiency), max(limits_df$maternal_education), max(limits_df$mcv1), max(limits_df$u5mr), max(limits_df$prop_urban), 0.05))
-
-  max_limits$origin <- ifelse(max_limits$variable == "vitA_deficiency", unique(d$vitA_deficiency_origin),
-                              ifelse(max_limits$variable == "maternal_education", unique(d$maternal_education_origin),
-                                     ifelse(max_limits$variable == "mcv1", unique(d$mcv1_origin),
-                                            ifelse(max_limits$variable == "u5mr", unique(d$u5mr_origin),
-                                                   ifelse(max_limits$variable == "prop_urban", unique(d$prop_urban_origin),
-                                                          ifelse(max_limits$variable == "incidence", unique(d$incidence_origin), NA))))))
-
-  max_limits <- subset(max_limits, origin == 'User-specified')
+                           Z = c(max(limits_df$vitA_deficiency), max(limits_df$maternal_education), max(limits_df$mcv1), max(limits_df$u5mr), max(limits_df$prop_urban), max(limits_df$incidence, na.rm=T)))
 
   ## set min limits for lines
   min_limits <- data.frame(variable = c("vitA_deficiency", "maternal_education", "mcv1", "u5mr", "prop_urban", "incidence"),
-                           Z = c(min(limits_df$vitA_deficiency), min(limits_df$maternal_education), min(limits_df$mcv1), min(limits_df$u5mr), min(limits_df$prop_urban), 0.0005))
-
-  min_limits$origin <- ifelse(min_limits$variable == "vitA_deficiency", unique(d$vitA_deficiency_origin),
-                              ifelse(min_limits$variable == "maternal_education", unique(d$maternal_education_origin),
-                                     ifelse(min_limits$variable == "mcv1", unique(d$mcv1_origin),
-                                            ifelse(min_limits$variable == "u5mr", unique(d$u5mr_origin),
-                                                   ifelse(min_limits$variable == "prop_urban", unique(d$prop_urban_origin),
-                                                          ifelse(min_limits$variable == "incidence", unique(d$incidence_origin), NA))))))
-
-  min_limits <- subset(min_limits, origin == 'User-specified')
+                           Z = c(min(limits_df$vitA_deficiency), min(limits_df$maternal_education), min(limits_df$mcv1), min(limits_df$u5mr), min(limits_df$prop_urban), min(limits_df$incidence, na.rm=T)))
 
   gg_ctry <- ggplot(d, aes(x = year, y = value)) + geom_line(color='grey28') + theme_bw() +
-    geom_point(aes(color=as.factor(value_origin))) + xlab("Year") + ylab("Value") +
+    geom_point(color='dodgerblue4') + xlab("Year") + ylab("Value") +
     geom_hline(data = max_limits, aes(yintercept = Z)) +
     geom_hline(data = min_limits, aes(yintercept = Z)) + ggtitle(country) +
-    facet_wrap(~ variable, scales = 'free') + scale_color_manual(values=c("Default" = "#1B9E77",
-                                                                          "User-specified" = "#D95F02")) +
+    facet_wrap(~ variable, scales = 'free') +
     guides(color=guide_legend(title=""))
-
 
   return(gg_ctry)
 }

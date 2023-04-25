0a659631d84dce93c2a8c0f4d195c4fa9b415c63
everdyke <everdyke@uw.edu>
2023-04-25 13:08:03 -0700

    pip install spmat

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@ mrtool <- NULL
   # library(reticulate)
   #install_miniconda()
   conda_create(envname = 'mrtool-0.1.0', python_version = '3.10')
-  py_install(envname = 'mrtool-0.1.0', packages = c('mrtool==0.1.0', 'dill'), pip = TRUE)
+  py_install(envname = 'mrtool-0.1.0', packages = c('mrtool==0.1.0', 'dill', 'spmat==0.0.9'), pip = TRUE)
   Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS='true');
   mrtool <<- reticulate::import("mrtool", delay_load = TRUE)
 

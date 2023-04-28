6352bc215d19ef96abed610c621790698c7ae354
everdyke <everdyke@uw.edu>
2023-04-28 13:35:24 -0700

    remove miniconda

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,6 @@ mrtool <- NULL
 
 .onLoad <- function(libname, pkgname) {
   library(reticulate)
-  reticulate::install_miniconda()
   conda_create(envname = 'mrtool-0.1.0', python_version = '3.10')
   py_install(envname = 'mrtool-0.1.0', packages = c('mrtool==0.1.0', 'dill', 'spmat==0.0.9'), pip = TRUE)
   use_condaenv(condaenv = "mrtool-0.1.0")
